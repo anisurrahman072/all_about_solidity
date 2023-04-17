12810cb4cfa5c11ec71b16b8652c917ee8de46a2
anisurrahman072 <anisurrahmanbup@gmail.com>
2023-04-17 14:03:24 +0600

    Assign-a-storage-to-STATE_will_create_REFERENCE

---
+++ Storage_Data_location/Assign-a-storage-to-STATE_will_create_REFERENCE.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.19;
+
+contract StorageDataLocation {
+    uint[] myAges = [1, 2, 3, 4];
+
+    function getMyAges() external view returns (uint[] memory) {
+        return myAges;
+    }
+
+    function updateMyAnoAges() external {
+        uint[] storage myAnoAges = myAges;
+        myAnoAges[0] = 100;
+        myAnoAges[1] = 200;
+        myAnoAges[2] = 300;
+        // The myAges STATE will also be in change
+    }
+}
