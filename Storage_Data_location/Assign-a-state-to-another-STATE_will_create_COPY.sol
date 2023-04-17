215a55b92c5981bd1e25efc47820a80a1181d124
anisurrahman072 <anisurrahmanbup@gmail.com>
2023-04-17 13:53:03 +0600

    Assign-a-state-to-another-STATE_will_create_COPY

---
+++ Storage_Data_location/Assign-a-state-to-another-STATE_will_create_COPY.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.19;
+
+contract StorageDataLocation {
+    uint[] myAges = [1, 2, 3, 4];
+    uint[] myAnoAges = myAges;
+
+    function getMyAges() external view returns (uint[] memory) {
+        return myAges;
+    }
+
+    function getMyAnoAges() external view returns (uint[] memory) {
+        return myAnoAges;
+    }
+
+    function updateMyAnoAges() external {
+        myAnoAges = [100, 200, 300]; // This will not update myAges ARRAY.
+    }
+}
